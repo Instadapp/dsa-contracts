120738ab2c87792e0c701f416d2fbe37f5cf4705
Thrilok Kumar <thrilok2000@gmail.com>
2020-02-09 17:44:15 +0530

    updated test scripts for account version control

--- test/1_index.js
+++ test/1_index.js
@@ -5,6 +5,11 @@ const accountContract = artifacts.require("InstaAccount")
 
 
 contract("InstaIndex", async (accounts) => {
+    let accountVersion;
+     before(async() => {
+        accountVersion = await getAccountVersion();
+    })
+    
     const masterOne = accounts[0];
     const masterTwo = accounts[1];
     const slaAccountOne = accounts[2];
@@ -15,7 +20,7 @@ contract("InstaIndex", async (accounts) => {
     it("Account.sol Contract deployed.", async () =>
     {   
         // check if account.sol contract deployed and set in index.sol
-        await getRegistryDetails()
+        await getRegistryDetails(accountVersion)
     })
 
     it("Accounts Contract's index variable is index contract address.", async () =>
@@ -33,7 +38,7 @@ contract("InstaIndex", async (accounts) => {
     it("Check changed.(from: checkOne, to: checkTwo, by:masterOne)", async () =>
     {   
         //change check address from index.sol from checkOne to checkTwo
-        await changeCheck(masterOne, checkTwo)
+        await changeCheck(masterOne, checkTwo, accountVersion)
     })
 
     it("Master changed.(from: masterOne, to: masterTwo)", async () =>
@@ -45,7 +50,7 @@ contract("InstaIndex", async (accounts) => {
     it("Check changed.(from: checkTwo, to: checkOne, by:masterTwo)", async () =>
     {
         //change check address from index.sol from checkTwo to checkOne
-        await changeCheck(masterTwo, checkOne)
+        await changeCheck(masterTwo, checkOne, accountVersion)
     })
 
     it("Master Changed.(From: masterTwo, to: masterTwo)", async () =>
@@ -57,13 +62,13 @@ contract("InstaIndex", async (accounts) => {
     it("SLA created for slaAccountOne", async () =>
     {   
         //Create single SLA account
-        await createSLA(slaAccountOne, accounts)
+        await createSLA(slaAccountOne, accounts, accountVersion)
     })
 
     it("Multiple SLA created for slaAccontTwo", async () =>
     {
         //Create multi SLA account
-        await createMultiSLA(slaAccountTwo)
+        await createMultiSLA(slaAccountTwo, accountVersion)
     })
 
     it("Master is masterOne.", async () =>
@@ -75,9 +80,9 @@ contract("InstaIndex", async (accounts) => {
 
   });
 
-async function getRegistryDetails() {
+async function getRegistryDetails(accountVersion) {
     var indexInstance = await indexContract.deployed(); // InstaIndex instance
-    var SLAaddr = await indexInstance.account(); // account address from index.sol
+    var SLAaddr = await indexInstance.account(accountVersion); // account address from index.sol
     assert.notEqual(SLAaddr, "0x0000000000000000000000000000000000000000")
 }
 
@@ -103,17 +108,17 @@ async function changeMaster(master, nextAdmin) {
     assert.notEqual(smartMasteraddr, master)
 }
 
-async function changeCheck(master, nextCheckAddr) {
+async function changeCheck(master, nextCheckAddr, accountVersion) {
     var indexInstance = await indexContract.deployed(); // InstaIndex instance
-    await indexInstance.changeCheck(nextCheckAddr, {from: master}); // change check address in index.sol contract
-    var checkAddr = await indexInstance.check(); // get 'check' address from index.sol contract
+    await indexInstance.changeCheck(accountVersion, nextCheckAddr, {from: master}); // change check address in index.sol contract
+    var checkAddr = await indexInstance.check(accountVersion); // get 'check' address from index.sol contract
     assert.equal(checkAddr, nextCheckAddr)
 }
 
-async function createSLA(owner, accounts) {
+async function createSLA(owner, accounts, accountVersion) {
     var indexInstance = await indexContract.deployed(); // InstaIndex instance
     var listInstance = await listContract.deployed(); // InstaList instance
-    await indexInstance.build(owner, owner, {from: owner}); // Create a new SLA account for `owner` 
+    await indexInstance.build(owner, accountVersion, owner, {from: owner}); // Create a new SLA account for `owner` 
     
     var slaOwnerLink = await listInstance.userLink(owner); // get owner details of SLA(IDs)
     // var slaFirstOwner = await listInstance.FirstOwner(slaAddr)
@@ -125,16 +130,20 @@ async function createSLA(owner, accounts) {
     assert.ok(isOwner);
 }
 
-async function createMultiSLA(owner) {
+async function createMultiSLA(owner, accountVersion) {
     var indexInstance = await indexContract.deployed(); // InstaIndex instance
     var listInstance = await listContract.deployed(); // InstaList instance
-    await indexInstance.build(owner, owner, {from: owner}); // Create a new SLA account for `owner`
-    await indexInstance.build(owner, owner, {from: owner}); // Create a new SLA account for `owner`
-    await indexInstance.build(owner, owner, {from: owner}); // Create a new SLA account for `owner`
+    await indexInstance.build(owner, accountVersion, owner, {from: owner}); // Create a new SLA account for `owner` 
+    await indexInstance.build(owner, accountVersion, owner, {from: owner}); // Create a new SLA account for `owner` 
+    await indexInstance.build(owner, accountVersion, owner, {from: owner}); // Create a new SLA account for `owner` 
     
     var slaOwnerLink = await listInstance.userLink(owner); // Get SLA Accounts details by `owner`
     assert.notEqual(slaOwnerLink.count, 0);
-    
+}
+
+async function getAccountVersion() {
+    var indexInstance = await indexContract.deployed(); // InstaIndex instance
+    return await indexInstance.versionCount();
 }
 
 function pause(ms) {
--- test/2_connectors.js
+++ test/2_connectors.js
@@ -8,12 +8,17 @@ contract("InstaConnectors", async (accounts) => {
     const master = accounts[0];
     const cheifOne = accounts[8];
 
+    let accountVersion;
+     before(async() => {
+        accountVersion = await getAccountVersion();
+    })
+
     it("Connector Contract is deployed And registred in registry.", async () =>
     {   
         // Checking if connectors.sol contract deployed. And
         // connectors.sol address is set in index.sol
         var indexInstance = await indexContract.deployed(); //InstaIndex instance
-        var ConnectorsIndexAddr = await indexInstance.connectors(); //get connectors.sol address variable from index.sol
+        var ConnectorsIndexAddr = await indexInstance.connectors(accountVersion); //get connectors.sol address variable from index.sol
         var ConnectorsAddr = await connectorsContract.deployed() // get address of deployed connectors.sol
         assert.equal(ConnectorsIndexAddr, ConnectorsAddr.address)
     })
@@ -21,7 +26,7 @@ contract("InstaConnectors", async (accounts) => {
     it("Connectors Contract's index variable is index contract address.", async () =>
     {       
         // Get index address variable from connectors.sol
-        await getConnectorsIndexAddress()
+        await getConnectorsIndexAddress(accountVersion)
     })
 
     it("Removed Basic connector.(From: master).", async () =>
@@ -105,6 +110,11 @@ async function enableConnector(cheif, connectorAddress) {
     await connectorInstance.enable(connectorAddress, {from:cheif}); // Enable the the given connector address
     var isEnabled = await connectorInstance.connectors(connectorAddress) // check if the give connector address is enabled.
     assert.ok(isEnabled)
+}
+
+async function getAccountVersion() {
+    var indexInstance = await indexContract.deployed(); // InstaIndex instance
+    return await indexInstance.versionCount();
 }
 
 
