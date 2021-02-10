10d4c4f3aa3cd8a1b9d6868384e1fcb119d55a8e
Mubaris NK <mubaris@instadapp.io>
2021-02-11 02:05:22 +0530

    revert transfer test methods

--- contracts/v2/connectors/test/emitEvent.test.sol
+++ contracts/v2/connectors/test/emitEvent.test.sol
@@ -1,9 +1,5 @@
 pragma solidity ^0.7.0;
 
-interface HolderInterface {
-    function ping() external;
-}
-
 contract ConnectV2EmitEvent {
 
     event LogEmitEvent(address indexed dsaAddress, address indexed _sender);
@@ -13,9 +9,5 @@ contract ConnectV2EmitEvent {
 
         _eventName = "LogEmitEvent(address,address)";
         _eventParam = abi.encode(address(this), msg.sender);
-    }
-
-    function claim(address addr) external payable {
-        HolderInterface(addr).ping();
     }
 }
