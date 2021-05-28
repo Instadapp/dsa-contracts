69eb554670c45680414c2fed992b077f0412133c
Aleksandr S <ardhead@gmail.com>
2021-05-28 17:54:01 +0300

    adress validation demo

--- scripts/testValidation.js
+++ scripts/testValidation.js
@@ -1,7 +1,10 @@
 const hre = require('hardhat')
-const { ethers, deployments } = hre
+const axios = require('axios')
+
+const { ethers, artifacts, deployments } = hre
 const fs = require('fs/promises')
 const path = require('path')
+const test = require('@openzeppelin/upgrades-core')
 const {
   assertUpgradeSafe,
   assertStorageUpgradeSafe,
@@ -14,18 +17,27 @@ const {
   getStorageLayoutForAddress, // TODO: How to use this?
   isCurrentValidationData,
   getCode,
-  extractStorageLayout, // TODO: How to use this?
+  extractStorageLayout // TODO: How to use this?
 } = require('@openzeppelin/upgrades-core')
 
 async function main () {
   try {
-    await checkForLocalMultipleImplementationStorage()
+    const address = '0x8a3462A50e1a9Fe8c9e7d9023CAcbD9a98D90021'
+    await checkForLocalMultipleImplementationStorage([
+      'InstaImplementationM1',
+      'InstaImplementationM2'
+    ])
+    const addressImpl = await findImplByAddress(address)
+    await checkForLocalMultipleImplementationStorage([
+      addressImpl,
+      'InstaImplementationM2'
+    ])
     // HOW TO MAKE THIS WORK?
-    const { provider } = hre.network
-    const manifest = await Manifest.forNetwork(provider)
-    const validations = await readValidations(hre)
-    const deploymentLayout = await getStorageLayoutForAddress(manifest, validations, "0x8a3462A50e1a9Fe8c9e7d9023CAcbD9a98D90021")
+    // const { provider } = hre.network
+    // const manifest = await Manifest.forNetwork(provider)
+    // const validations = await readValidations(hre)
     // // let ImplFactory2 = await getCode(provider, "0x8a3462A50e1a9Fe8c9e7d9023CAcbD9a98D90021")
+    // const deploymentLayout = await getStorageLayoutForAddress(manifest, validations, address)
     // // // console.log("main -> Impl", ImplFactory2)
     // // ImplFactory2 = {bytecode: ImplFactory2}
     // // // const ImplFactory = await ethers.getContractFactory('InstaImplementationM1') // need name?
@@ -45,24 +57,24 @@ async function main () {
     // // const layout = getStorageLayout(validations, version)
     // const layout2 = getStorageLayout(validations, version2)
     // // assertStorageUpgradeSafe(layout2, layout, false)
-    // console.log('passed')
+    console.log('main passed')
   } catch (error) {
     console.log('not passed', error)
   }
 }
 
-async function checkForLocalMultipleImplementationStorage() {
+async function checkForLocalMultipleImplementationStorage (implArr) {
   try {
     const validations = await readValidations(hre)
-    const InstaImplementationM1 = await ethers.getContractFactory('InstaImplementationM1')
-    const InstaImplementationM2 = await ethers.getContractFactory('InstaImplementationM2')
-    const unlinkedBytecodeM1 = getUnlinkedBytecode(validations, InstaImplementationM1.bytecode)
-    const unlinkedBytecode2M2 = getUnlinkedBytecode(validations, InstaImplementationM2.bytecode)
-    const versionM1 = getVersion(unlinkedBytecodeM1, InstaImplementationM1.bytecode)
-    const versionM2 = getVersion(unlinkedBytecode2M2, InstaImplementationM2.bytecode)
-    const layoutM1 = getStorageLayout(validations, versionM1)
-    const layoutM2 = getStorageLayout(validations, versionM2)
-    assertStorageUpgradeSafe(layoutM1, layoutM2, false)
+    implArr = implArr.map(val => ({ name: val }))
+    for (let index = 0; index < implArr.length; index++) {
+      const impl = implArr[index]
+      impl.factory = await ethers.getContractFactory(impl.name)
+      impl.unlinkedBytecode = getUnlinkedBytecode(validations, impl.factory.bytecode)
+      impl.version = getVersion(impl.unlinkedBytecode, impl.factory.bytecode)
+      impl.layout = getStorageLayout(validations, impl.version)
+    }
+    assertStorageUpgradeSafe(implArr[0].layout, implArr[1].layout, false)
     console.log('passed')
   } catch (error) {
     console.log('not passed', error)
@@ -72,9 +84,9 @@ async function checkForLocalMultipleImplementationStorage() {
 main()
   .then(() => process.exit(0))
   .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
 class ValidationsCacheNotFound extends Error {
   constructor () {
     super('Validations cache not found. Recompile with `hardhat compile --force`')
@@ -107,4 +119,30 @@ async function readValidations (hre) {
       throw e
     }
   }
+}
+
+async function getEtherscanSourceCode (address) {
+  try {
+    const responce = await axios.get('https://api.etherscan.io/api', {
+      params: {
+        module: 'contract',
+        action: 'getsourcecode',
+        address,
+        apikey: process.env.ETHERSCAN
+      }
+    })
+    return responce.data.result[0].SourceCode
+  } catch (error) {
+    Promise.reject(error)
+  }
+}
+
+async function findImplByAddress (address) {
+  const code = await getEtherscanSourceCode(address)
+  const path = code.split('contracts')[1].split('"')[0]
+  const contractCode = await fs.readFile(`./contracts${path}`, { encoding: 'utf8' })
+  const implName = contractCode
+    .slice(contractCode.lastIndexOf('contract ') + 9)
+    .split(' ')[0]
+  return implName
 }
