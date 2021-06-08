2693e853c90dc7dece823523b0e289bf3de4f7b7
Daksh Miglani <hello@dak.sh>
2021-06-09 01:09:08 +0530

    test: 💍 try adding tests for flasloans

--- scripts/getDeployedContracts.js
+++ scripts/getDeployedContracts.js
@@ -2,35 +2,37 @@ const abis = require("./constant/abis");
 const addresses = require("./constant/addresses");
 const { expect } = require("chai");
 
-
 const hre = require("hardhat");
 const { ethers, deployments } = hre;
-const fs = require("fs/promises")
-
+const fs = require("fs/promises");
 
 module.exports = async function () {
-    const chainId = hre.network.config.chainId
-    const contracts = JSON.parse(await fs.readFile('docs/addresses.json', { encoding: 'utf8' }))
-    const network = Object.keys(contracts)
-    if (!network[chainId]) throw new Error("Contacts for Network not found")
-    const destructured = flattenObject(contracts[chainId])
+  const chainId = hre.network.config.chainId;
+  const contracts = JSON.parse(
+    await fs.readFile("docs/addresses.json", { encoding: "utf8" })
+  );
+  const network = Object.keys(contracts);
+  const isPolygon = chainId === 137;
+  const destructured = flattenObject(
+    contracts[isPolygon ? "polygon" : "mainnet"]
+  );
 
-    const contractInstances = {}
-    for (const key in destructured) {
-        contractInstances[key] = await ethers.getContractAt(key, destructured[key])
-    }
+  const contractInstances = {};
+  for (const key in destructured) {
+    contractInstances[key] = await ethers.getContractAt(key, destructured[key]);
+  }
 
-    return contractInstances;
+  return contractInstances;
 };
 
 const flattenObject = (obj) => {
-    const flattened = {}
-    Object.keys(obj).forEach((key) => {
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-        Object.assign(flattened, flattenObject(obj[key]))
-      } else {
-        flattened[key] = obj[key]
-      }
-    })
-    return flattened
-}
+  const flattened = {};
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      Object.assign(flattened, flattenObject(obj[key]));
+    } else {
+      flattened[key] = obj[key];
+    }
+  });
+  return flattened;
+};
