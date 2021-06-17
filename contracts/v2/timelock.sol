219ba25f295bfb65a8fa9f391a0df63ecf5705e6
Thrilok Kumar <thrilok2000@gmail.com>
2021-06-18 01:32:56 +0530

    Added timelock contract

---
+++ contracts/v2/timelock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/access/TimelockController.sol";
+
+interface IndexInterface {
+    function master() external view returns (address);
+    function changeMaster(address) external;
+    function updateMaster() external;
+}
+
+contract InstaTimelockContract is TimelockController {
+
+    IndexInterface constant public instaIndex = IndexInterface(0x2971AdFa57b20E5a416aE5a708A8655A9c74f723);
+    address constant public governanceTimelock = 0xC7Cb1dE2721BFC0E0DA1b9D526bCdC54eF1C0eFC;
+    
+    constructor() public {
+        address[] memory masterSig = new address[](1);
+        masterSig[i] = instaIndex.master();
+        uint256 minDelay = 2;
+        TimelockController(minDelay, masterSig, masterSig);
+        instaIndex.updateMaster();
+        instaIndex.changeMaster(governanceTimelock);
+    }
+}
