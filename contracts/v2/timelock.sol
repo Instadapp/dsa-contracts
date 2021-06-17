1700332b74d0121d66e97fe10fd5eaa9f1eebe41
Thrilok Kumar <thrilok2000@gmail.com>
2021-06-18 01:33:25 +0530

    minDelay => 3 days

--- contracts/v2/timelock.sol
+++ contracts/v2/timelock.sol
@@ -13,11 +13,11 @@ contract InstaTimelockContract is TimelockController {
 
     IndexInterface constant public instaIndex = IndexInterface(0x2971AdFa57b20E5a416aE5a708A8655A9c74f723);
     address constant public governanceTimelock = 0xC7Cb1dE2721BFC0E0DA1b9D526bCdC54eF1C0eFC;
-    
+
     constructor() public {
         address[] memory masterSig = new address[](1);
         masterSig[i] = instaIndex.master();
-        uint256 minDelay = 2;
+        uint256 minDelay = 3 days;
         TimelockController(minDelay, masterSig, masterSig);
         instaIndex.updateMaster();
         instaIndex.changeMaster(governanceTimelock);
