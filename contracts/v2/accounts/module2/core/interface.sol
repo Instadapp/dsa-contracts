549b7eb74eedaf96013c73e13621d9f20cfa9e97
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2021-08-21 22:59:44 +0530

    minor routing update on interface

--- contracts/v2/accounts/module2/core/interface.sol
+++ contracts/v2/accounts/module2/core/interface.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.7.0;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20 } from "../../common/IERC20.sol";
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
