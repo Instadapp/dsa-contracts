716efca8f2320c9b81e3d537c1809cb79e0c5bfa
cryptoDev222 <genius.developer.63@gmail.com>
2021-08-26 12:30:26 -0500

    ERC1155 Receiver and test case file

--- contracts/v2/accounts/default/implementation_default.sol
+++ contracts/v2/accounts/default/implementation_default.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import { Variables } from "../variables.sol";
+import {Variables} from "../variables.sol";
 
 interface IndexInterface {
     function list() external view returns (address);
@@ -9,15 +9,16 @@ interface IndexInterface {
 
 interface ListInterface {
     function addAuth(address user) external;
+
     function removeAuth(address user) external;
 }
 
 contract Constants is Variables {
-    uint public constant implementationVersion = 1;
+    uint256 public constant implementationVersion = 1;
     // InstaIndex Address.
     address public immutable instaIndex;
     // The Account Module Version.
-    uint public constant version = 2;
+    uint256 public constant version = 2;
 
     constructor(address _instaIndex) {
         instaIndex = _instaIndex;
@@ -41,9 +42,12 @@ contract Record is Constants {
     /**
      * @dev Enable New User.
      * @param user Owner address
-    */
+     */
     function enable(address user) public {
-        require(msg.sender == address(this) || msg.sender == instaIndex, "not-self-index");
+        require(
+            msg.sender == address(this) || msg.sender == instaIndex,
+            "not-self-index"
+        );
         require(user != address(0), "not-valid");
         require(!_auth[user], "already-enabled");
         _auth[user] = true;
@@ -54,7 +58,7 @@ contract Record is Constants {
     /**
      * @dev Disable User.
      * @param user Owner address
-    */
+     */
     function disable(address user) public {
         require(msg.sender == address(this), "not-self");
         require(user != address(0), "not-valid");
@@ -64,10 +68,41 @@ contract Record is Constants {
         emit LogDisableUser(user);
     }
 
+    /**
+    * @dev ERC721 token receiver
+    */
+    function onERC721Received(address, address, uint256, bytes calldata) external returns (bytes4) {
+        return 0x150b7a02; // keccak256("onERC721Received(address,address,uint256,bytes)")
+    }
+
+    /**
+     * @dev ERC1155 token receiver
+     */
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory
+    ) public pure virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    /**
+     * @dev ERC1155 token receiver
+     */
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) public pure virtual returns (bytes4) {
+        this.onERC1155BatchReceived.selector;
+    }
 }
 
 contract InstaDefaultImplementation is Record {
-
     constructor(address _instaIndex) public Record(_instaIndex) {}
 
     receive() external payable {}
