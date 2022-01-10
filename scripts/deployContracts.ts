7c4d91fb7ca7e9400c18300201d01f4e815bbfad
pradyuman-verma <pradyumnverma27@gmail.com>
2022-01-10 18:20:37 +0530

    removed repeition: deployContract.ts

--- scripts/deployContracts.ts
+++ scripts/deployContracts.ts
@@ -2,56 +2,54 @@ import hre from "hardhat";
 const { ethers } = hre;
 import addresses from "./constant/addresses";
 
-const INSTA_INDEX = addresses.InstaIndex[String(process.env.networkType)];
+const networkType = String(process.env.networkType) ?? "mainnet";
+const INSTA_INDEX = addresses.InstaIndex[networkType];
+
+async function instaDeployContract(
+  factoryName: string,
+  constructorArguments: Array<string>
+) {
+  const contractInstance = await ethers.getContractFactory(factoryName);
+  const contract = await contractInstance.deploy([...constructorArguments]);
+  await contract.deployed();
+
+  console.log(
+    `Contract ${factoryName} deployed at ${contract.address} on ${hre.network.name}`
+  );
+
+  return contract.address;
+}
 
 export default async function () {
   const instaIndex = await ethers.getContractAt("InstaIndex", INSTA_INDEX);
 
-  const InstaConnectorsV2 = await ethers.getContractFactory(
-    "InstaConnectorsV2"
-  );
-  const instaConnectorsV2 = await InstaConnectorsV2.deploy(instaIndex.address);
-  await instaConnectorsV2.deployed();
-
-  const InstaImplementations = await ethers.getContractFactory(
-    "InstaImplementations"
-  );
-  const implementationsMapping = await InstaImplementations.deploy(
-    instaIndex.address
-  );
-  await implementationsMapping.deployed();
-
-  const InstaAccountV2 = await ethers.getContractFactory("InstaAccountV2");
-  const instaAccountV2Proxy = await InstaAccountV2.deploy(
-    implementationsMapping.address
-  );
-  await instaAccountV2Proxy.deployed();
-
-  const InstaDefaultImplementation = await ethers.getContractFactory(
-    "InstaDefaultImplementation"
-  );
-  const instaAccountV2DefaultImpl = await InstaDefaultImplementation.deploy(
-    instaIndex.address
-  );
-  await instaAccountV2DefaultImpl.deployed();
-
-  const InstaImplementationM1 = await ethers.getContractFactory(
-    "InstaImplementationM1"
-  );
-  const instaAccountV2ImplM1 = await InstaImplementationM1.deploy(
+  const instaConnectorsV2 = await instaDeployContract("InstaConnectorsV2", [
     instaIndex.address,
-    instaConnectorsV2.address
-  );
-  await instaAccountV2ImplM1.deployed();
+  ]);
 
-  const InstaImplementationM2 = await ethers.getContractFactory(
-    "InstaImplementationM2"
+  const implementationsMapping = await instaDeployContract(
+    "InstaImplementations",
+    [instaIndex.address]
   );
-  const instaAccountV2ImplM2 = await InstaImplementationM2.deploy(
-    instaIndex.address,
-    instaConnectorsV2.address
+
+  const instaAccountV2Proxy = await instaDeployContract("InstaAccountV2", [
+    implementationsMapping,
+  ]);
+
+  const instaAccountV2DefaultImpl = await instaDeployContract(
+    "InstaDefaultImplementation",
+    [instaIndex.address]
   );
-  await instaAccountV2ImplM2.deployed();
+
+  const instaAccountV2ImplM1 = await instaDeployContract(
+    "InstaImplementationM1",
+    [instaIndex.address, instaConnectorsV2]
+  );
+
+  const instaAccountV2ImplM2 = await instaDeployContract(
+    "InstaImplementationM2",
+    [instaIndex.address, instaConnectorsV2]
+  );
 
   return {
     instaIndex,
