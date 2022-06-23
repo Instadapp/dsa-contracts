d418d84522581f47beb73d276e55c042f9d197b0
Richa-iitr <richa@cs.iitr.ac.in>
2022-06-23 20:10:47 +0530

    added test check module

---
+++ contracts/v1/test/check.test.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract Change {
+    bool internal status;
+    function change(bool _status) public {
+        status = _status;
+    }
+}
+contract InstaCheck is Change {
+    function isOk() external view returns (bool ok) {
+        return status;
+    }
+}
